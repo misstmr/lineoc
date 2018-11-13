045fd3efa3187dbe1dd58b370633cf78de0ec313
sittipong.m <sittipong.m@sittipongms-MacBook-Air.local>
2018-11-14 01:14:17 +0700

    add

--- bot.php
+++ bot.php
@@ -39,7 +39,7 @@ if (!is_null($events['events'])) {
                            $i=0;
                            foreach ($alert as  $temp) {
                             $i++;
-$code = 'U+1F621';
+$code = '\xF0\x9F\x98\xA1';
 $msgData =  '-->RED! '.$code.$temp->kpi_id.'-'.$temp->kpi_name.'( '.$temp->kpi_value.' '.' Target='.$temp->Target2.')';
         
 
