c1f9630e26dd02e993f7383d3a3ec4d5c5a55bf8
mrmisstmr <mr.misstmr@gmail.com>
2019-01-18 12:22:16 +0700

    add

--- bot.php
+++ bot.php
@@ -76,7 +76,7 @@ if (!is_null($events['events'])) {
 
                   
                     $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+                    $url = 'https://api.line.me/v2/bot/message/push';
                     $ch = curl_init($url);
                     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
